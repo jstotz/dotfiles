5cf9ebd4e14681b05dd7197cf46573e41bc3fb7f
Jay Stotz <jason.stotz@gmail.com>
2017-03-16 15:32:50 -0400

    Shellcheck fixes

--- gcloud/completion.zsh
+++ gcloud/completion.zsh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# shellcheck disable=SC1090,SC2086
 test -f ~/google-cloud-sdk/completion.zsh.inc && . $_
--- gcloud/path.zsh
+++ gcloud/path.zsh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# shellcheck disable=SC1090,SC2086
 test -f ~/google-cloud-sdk/path.zsh.inc && . $_
