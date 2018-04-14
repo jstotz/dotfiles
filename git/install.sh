0a04e03e375376d4b480011989cd5850d9fff4d8
Jay Stotz <jason.stotz@gmail.com>
2018-04-14 16:55:51 -0400

    Install hub zsh completions

--- git/install.sh
+++ git/install.sh
@@ -11,3 +11,7 @@ fi
 if which diff-so-fancy > /dev/null 2>&1; then
   git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 fi
+
+if [ -f $GOPATH/src/github.com/github/hub/etc/hub.zsh_completion ]; then
+  ln -sf $GOPATH/src/github.com/github/hub/etc/hub.zsh_completion ~/.zsh/completions/_hub
+fi
