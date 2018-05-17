8eaa656eb4b409393af2054a07c29c0714d167cb
Jay Stotz <jason.stotz@gmail.com>
2018-05-17 13:14:08 -0400

    Custom i3status config

--- i3/install.sh
+++ i3/install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 [ "$(uname -s)" = "Darwin" ] && exit 0
+
 mkdir -p ~/.config/i3/
 ln -sf "$DOTFILES"/i3/config ~/.config/i3/config
+
+mkdir -p ~/.config/i3status/
+ln -sf "$DOTFILES"/i3/i3status/config ~/.config/i3status/config
