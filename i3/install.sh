641aa2ffb3870e5148dada82c68df54e35b9eb71
Jay Stotz <jason.stotz@gmail.com>
2018-05-17 13:43:55 -0400

    install x config files to set DPI

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 [ "$(uname -s)" = "Darwin" ] && exit 0
 
+ln -sf "$DOTFILES"/i3/xinitrc ~/.xinitrc
+ln -sf "$DOTFILES"/i3/Xresources ~/.Xresources
+
 mkdir -p ~/.config/i3/
 ln -sf "$DOTFILES"/i3/config ~/.config/i3/config
 
