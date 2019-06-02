962052f90726957d9ab9ac31a0669ee4113a6c68
Jay Stotz <jason.stotz@gmail.com>
2019-06-02 17:09:26 -0400

    Improve playerctl module

--- polybar/modules/playerctl.sh
+++ polybar/modules/playerctl.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-status=$(playerctl status) || exit $?
+status="$(playerctl status 2>/dev/null)" || exit $?
+
+title=$(exec playerctl metadata xesam:title)
+artist=$(exec playerctl metadata xesam:artist)
 
 if	[ "$status" = "Playing" ]; then
-    title=$(exec playerctl metadata xesam:title)
-    artist=$(exec playerctl metadata xesam:artist)
-	( echo "$title - $artist  " ) &
+  echo "$title - $artist  "
 elif [ "$status" = "Paused" ]; then
-    title=$(exec playerctl metadata xesam:title)
-    artist=$(exec playerctl metadata xesam:artist)
-	( echo "$title - $artist  " ) &
+  echo "$title - $artist  "
 fi
