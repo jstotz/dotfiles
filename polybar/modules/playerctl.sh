47b0291b4986adf098d04d7d527f326fc407b62b
Jay Stotz <jason.stotz@gmail.com>
2019-07-15 13:30:23 -0400

    Use spotify

--- polybar/modules/playerctl.sh
+++ polybar/modules/playerctl.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-status="$(playerctl status 2>/dev/null)" || exit $?
+status="$(playerctl -p spotify status 2>/dev/null)" || exit $?
 
-title=$(exec playerctl metadata xesam:title)
-artist=$(exec playerctl metadata xesam:artist)
-song_descr="$artist - $title"
+song_descr=$(playerctl -p spotify metadata -f "{{xesam:artist}} - {{xesam:title}}")
 
 if	[ "$status" = "Playing" ]; then
   echo "$song_descr"
