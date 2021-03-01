b82ef815de6061f8ada9b2598dd1d7ffe39540cc
Jay Stotz <jason.stotz@gmail.com>
2021-03-01 14:38:30 -0500

    Mac homebrew

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -9,3 +9,13 @@ test -d /home/linuxbrew/.linuxbrew && {
   export MANPATH="/home/linuxbrew/.linuxbrew/share/man${MANPATH+:$MANPATH}:"
   export INFOPATH="/home/linuxbrew/.linuxbrew/share/info${INFOPATH+:$INFOPATH}"
 }
+
+test -d /usr/local/Homebrew && {
+  # Generated with `brew shellenv`:
+  export HOMEBREW_PREFIX="/usr/local";
+  export HOMEBREW_CELLAR="/usr/local/Cellar";
+  export HOMEBREW_REPOSITORY="/usr/local/Homebrew";
+  export PATH="/usr/local/bin:/usr/local/sbin${PATH+:$PATH}";
+  export MANPATH="/usr/local/share/man${MANPATH+:$MANPATH}:";
+  export INFOPATH="/usr/local/share/info${INFOPATH+:$INFOPATH}";
+}
