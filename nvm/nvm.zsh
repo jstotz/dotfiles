2d162af1fefb0e7d2a9de0251648ea0e5458a7cc
Jay Stotz <jason.stotz@gmail.com>
2021-03-01 14:53:46 -0500

    Make nvm async

--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,4 +1,8 @@
-export NVM_DIR="$HOME/.nvm"
+function load_nvm() {
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+}
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
+async_start_worker nvm_worker -n
+async_register_callback nvm_worker load_nvm
+async_job nvm_worker sleep 0.1
