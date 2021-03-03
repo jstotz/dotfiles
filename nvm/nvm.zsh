823cc410927c28b830518bbfa9f8d22bb2a1d75b
Jay Stotz <jason.stotz@gmail.com>
2021-03-03 14:26:11 -0500

    Remove nvm

--- nvm/nvm.zsh
+++
@@ -1,8 +0,0 @@
-function load_nvm() {
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
-}
-
-async_start_worker nvm_worker -n
-async_register_callback nvm_worker load_nvm
-async_job nvm_worker sleep 0.1
