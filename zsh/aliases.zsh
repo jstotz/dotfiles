6199aad435c7ffa2312f82e9120702b7394b4f54
Jay Stotz <jason.stotz@gmail.com>
2021-03-03 14:27:13 -0500

    Sync some zsh files with upstream

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,2 @@
 #!/bin/sh
-__reload_dotfiles() {
-  PATH="$(command -p getconf PATH):/usr/local/bin"
-  # shellcheck disable=SC1090
-  . ~/.zshrc
-  cd . || return 1
-}
-alias reload!='__reload_dotfiles'
+alias reload!='exec "$SHELL" -l'
--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -3,8 +3,3 @@
 for topic_folder ($DOTFILES/*) if [ -d "$topic_folder" ]; then
   fpath=($topic_folder $fpath)
 fi
-
-mkdir -p ~/.zsh/completions
-
-fpath=(~/.zsh/completions $fpath)
-
