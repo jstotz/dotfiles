fe9918f2a4ebd53e473eb8798fd501ebe770de91
Jay Stotz <jason.stotz@gmail.com>
2021-03-03 14:26:03 -0500

    Update antibody install

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-if which brew >/dev/null 2>&1; then
-  brew install getantibody/tap/antibody || brew upgrade antibody
+if command -v brew >/dev/null 2>&1; then
+	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
+	brew install antibody
 else
-  curl -sL https://git.io/antibody | sh -s
+	curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
 fi
-
-antibody bundle < "$DOTFILES/antibody/bundles.txt" > ~/.bundles.txt
+antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
+antibody update
