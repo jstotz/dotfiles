605c3725960c7234ff2c2f8ad83d1bb550d0cf28
Jay Stotz <jason.stotz@gmail.com>
2021-03-04 17:20:42 -0500

    Remove vim config

--- vim/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-test -f ~/.vim/autoload/plug.vim || {
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-}
-
-echo "Installing vim plugins..."
-vim +PlugInstall +qall >/tmp/vim-plugins.log 2>&1 || true
