5e70b6b3f5f6c0857c3168a346bb00e1154cbe01
Jay Stotz <jay@release.com>
2023-03-14 12:26:39 -0400

    Use astrocommunity

---
+++ neovim/config/plugins/community.lua
@@ -0,0 +1,10 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+}
