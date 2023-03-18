ed698850b77965b1713f7d0b9cff1773560624c5
Jay Stotz <jay@release.com>
2023-03-18 16:45:20 -0400

    Add trouble and lsp_lines plugins

---
+++ neovim/config/diagnostics.lua
@@ -0,0 +1,4 @@
+return {
+  -- Disable virtual_text since it's redundant with lsp_lines.
+  virtual_text = false,
+}
--- neovim/config/plugins/community.lua
+++ neovim/config/plugins/community.lua
@@ -7,4 +7,6 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
 }
