1fbd4079180b800c40c444f46190d78f9867bcd1
Jay Stotz <jay@release.com>
2024-01-24 10:22:28 -0500

    Use a custom up arrow handler for atuin

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -64,53 +64,13 @@ bindkey -v
 # Reduce delay when switching between vi modes
 export KEYTIMEOUT=1
 
-# [PageUp] - Up a line of history
-if [[ ! -z "$terminfo[kpp]" ]]; then
-  bindkey "$terminfo[kpp]" up-line-or-history
-fi
-# [PageDown] - Down a line of history
-if [[ ! -z "$terminfo[knp]" ]]; then
-  bindkey "$terminfo[knp]" down-line-or-history
-fi
-# start typing + [Up-Arrow] - fuzzy find history forward
-if [[ ! -z "$terminfo[kcuu1]" ]]; then
-  bindkey "$terminfo[kcuu1]" history-substring-search-up
-fi
-# start typing + [Down-Arrow] - fuzzy find history backward
-if [[ ! -z "$terminfo[kcud1]" ]]; then
-  bindkey "$terminfo[kcud1]" history-substring-search-down
-fi
-if [[ ! -z "$terminfo[khome]" ]]; then
-  # [Home] - Go to beginning of line
-  bindkey "$terminfo[khome]" beginning-of-line
-  # OPTION+left
-  bindkey '[D' beginning-of-line
-fi
-if [[ ! -z "$terminfo[kend]" ]]; then
-  # [End] - Go to end of line
-  bindkey "$terminfo[kend]"  end-of-line
-  # OPTION+right
-  bindkey '[C' end-of-line
-fi
-
-# [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5C' forward-word
-# [Ctrl-LeftArrow] - move backward one word
-bindkey '^[[1;5D' backward-word
-# [Shift-Tab] - move through the completion menu backwards
-if [[ ! -z "$terminfo[kcbt]" ]]; then
-  bindkey "$terminfo[kcbt]" reverse-menu-complete
-fi
-# [Backspace] - delete backward
-bindkey '^?' backward-delete-char
-# [Delete] - delete forward
-if [[ ! -z "$terminfo[kdch1]" ]]; then
-  bindkey "$terminfo[kdch1]" delete-char
-else
-  bindkey "^[[3~" delete-char
-  bindkey "^[3;5~" delete-char
-  bindkey "\e[3~" delete-char
-fi
-
 # ALT-C - cd into the selected directory
 bindkey '\ec' fzf-cd-widget
+
+# load atuin (shell history)
+eval "$(atuin init zsh --disable-up-arrow)"
+bindkey -M vicmd 'k' atuin-history-up
+bindkey -M vicmd 'j' atuin-history-down
+bindkey "${key[Up]}" atuin-history-up
+bindkey "${key[Down]}" atuin-history-down
+
