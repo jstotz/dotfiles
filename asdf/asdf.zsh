e47b0c5c6cbe06c0bb396d29db6dc2282fc9c0d8
Jay Stotz <jay@release.com>
2024-06-04 14:23:43 -0400

    Update asdf config

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -3,3 +3,7 @@
 if type asdf &> /dev/null; then
   . "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh"
 fi
+
+test -e ~/.asdf/plugins/golang/set-env.zsh && . $_
+
+export ASDF_GOLANG_MOD_VERSION_ENABLED=true
