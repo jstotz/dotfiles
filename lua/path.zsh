a92d18b66aee612a0e7d28a3e36388f4389eda45
Jay Stotz <jay@release.com>
2024-06-04 14:23:21 -0400

    Add lua path

---
+++ lua/path.zsh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PATH="~/.luarocks/bin:$PATH"
+export LUA_PATH='/usr/local/Cellar/luarocks/3.11.0/share/lua/5.4/?.lua;/usr/local/share/lua/5.4/?.lua;/usr/local/share/lua/5.4/?/init.lua;/usr/local/lib/lua/5.4/?.lua;/usr/local/lib/lua/5.4/?/init.lua;./?.lua;./?/init.lua;/Users/jstotz/.luarocks/share/lua/5.4/?.lua;/Users/jstotz/.luarocks/share/lua/5.4/?/init.lua'
+export LUA_CPATH='/usr/local/lib/lua/5.4/?.so;/usr/local/lib/lua/5.4/loadall.so;./?.so;/Users/jstotz/.luarocks/lib/lua/5.4/?.so'
