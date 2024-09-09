3828d6c58c6d1edfccafcf323e082323b6a4aaf2
Jay Stotz <jason.stotz@gmail.com>
2024-09-09 14:19:22 -0400

    Install gcloud SDK via Homebrew

--- gcloud/completion.zsh
+++ gcloud/completion.zsh
@@ -2,3 +2,4 @@
 
 # shellcheck disable=SC1090,SC2086
 test -f ~/google-cloud-sdk/completion.zsh.inc && . $_
+test -f /usr/local/share/google-cloud-sdk/completion.zsh.inc && . $_
--- gcloud/path.zsh
+++ gcloud/path.zsh
@@ -2,3 +2,4 @@
 
 # shellcheck disable=SC1090,SC2086
 test -f ~/google-cloud-sdk/path.zsh.inc && . $_
+test -f /usr/local/share/google-cloud-sdk/path.zsh.inc && . $_
